50d977cfb8c366685242d502c3f4ea010666fd6a
SimonArdrey <simon.ardrey@psychwire.org>
2017-09-07 11:38:45 +1000

    Installed and fixed active admin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require_directory .
+//= require_directory ./channels
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_admin!
+    return true
+
+    if current_user && current_user.is_admin
+      # fine
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
