0980d09c5ad925cfae30dbc50accf45abae321a5
SimonArdrey <simon.ardrey@psychwire.org>
2017-09-19 20:40:10 +1000

    Begin profile page and more view tinkering

--- app/controllers/concerns/user_profile.rb
+++ app/controllers/concerns/user_profile.rb
@@ -3,14 +3,21 @@ module UserProfile
 
   included do
     prepend_before_action :set_user
+    before_action :set_profile_info
     layout "profile"
   end
 
   def set_user
     if params[:user_slug]
-      @user = User.find_by_slug(params[:user_slug])
+      @user = User.find_by_param(params[:user_slug])
     elsif params[:slug]
-      @user = User.find_by_slug(params[:slug])
+      @user = User.find_by_param(params[:slug])
     end
+  end
+
+  def set_profile_info
+    return unless @user
+
+    @user_post_count = @user.author_posts.count
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   include UserProfile
 
+  def index
+    @users = User.all
+    render layout: "application"
+  end
+
   def show
+    @postable = @user
+    @posts = @user.author_posts
+      .includes(:postable, :user)
+      .order(created_at: :desc)
+      .limit(20)
+      @post = Post.new
+      @post.user = current_user
   end
 end
