b5f82649212115ec085e0400d98bcce459d71944
SimonArdrey <simon.ardrey@psychwire.org>
2017-09-19 14:03:24 +1000

    Add names and slug to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907034407) do
+ActiveRecord::Schema.define(version: 20170915020703) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -72,6 +72,9 @@ ActiveRecord::Schema.define(version: 20170907034407) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
