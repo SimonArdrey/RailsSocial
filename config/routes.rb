f4d096c1440664b07b22acfca3c374b00310ad91
Simon Ardrey <simonardrey@gmail.com>
2017-11-08 14:54:56 +1000

    Limited resource actions for /posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, path: "/account", controllers: { registrations: "users/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  resources :posts, only: [:index, :show, :delete, :update]
 
   resources :users, path: "profiles", param: :slug, :constraints => { :slug => /[^\/]+/ } do
     resources :posts, controller: "users_posts"
